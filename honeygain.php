629eca5f668bb13a571807bac7205c0e67553ea0
Marek Guráň <skemgi2@gmail.com>
2024-04-10 10:32:38 +0200

    small update

---
+++ honeygain.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="sk">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HoneyGain na SK</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="./assets/css/honeygain.css">
+    <link href="fonts/fontawesome-free-6.5.1-web/css/all.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <meta name="description"
+        content="Oplatí sa HoneyGain na slovensku? Čo je to HoneyGain? Ako zarobiť pasívne s HoneyGain?">
+    <meta name="keywords" content="HoneyGain, Slovensko, pasívny income, peniaze, zárobok">
+</head>
+
+<body class="bg-light">
+    <div class="container py-5">
+        <div class="row h-100">
+            <div class="col-md-6 d-flex flex-column justify-content-center">
+                <h1 class="mb-3">Čo je to HoneyGain?</h1>
+                <p class="text-muted">HoneyGain je služba, ktorá vám pasívne zarába peniaze poskytovaním
+                    firmám vašu
+                    IP adresu, ktorú
+                    využívajú na kontrolu SEO, cien produktov v iných krajinách, na marketingové účely a iného.<br>
+                    <br>
+                    HoneyGain taktiež zabezpečuje, že <strong>vaše dáta sú v bezpečí a nebudú zneužité.</strong> Odkazuje na to, že ich
+                    klienti sú <strong>overení a dôveryhodní.</strong><br>
+                    <br>
+                    Každý deň máte možnosť <strong>vyhrať,</strong> ak prenesiete <strong>15 MB</strong> dát od <strong>1 bodu po 100 000 bodov,</strong> čo predstavuje <strong>100
+                    dolárov.</strong> Tieto peniaze a vaše zárobky si môžete vybrať cez <strong>PayPal alebo v kryptomenách.</strong><br>
+                </p>
+            </div>
+            <div class="col-md-6 d-flex">
+                <div class="embed-responsive w-100 h-100">
+                    <iframe class="embed-responsive-item" style="width: 100%; height: 100%; border-radius: 18px!important; box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.15);"
+                        src="https://www.youtube.com/embed/PSByosJe17s" title="What is Honeygain? Passive income app"
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+        <h1 class="mt-5 mb-3">Oplatí sa to na slovensku?</h1>
+        <p class="text-muted">Záleží od toho či budete zdieľať svoje internetové pripojenie <strong>24/7, 3 hodiny denne alebo
+            jeden deň v týždni.</strong> Samozrejme, že sa najviac oplatí 24/7 ale za celý deň toho moc nezarobíte. Forma
+            zdieľania vášho internetu s
+            HoneyGain je <strong>dlhodobá záležitosť.</strong><br><br>
+            Môžete na to použiť aj <strong>mobilné zariadenia</strong> (iOS, Android) alebo aj <strong>počítače</strong> (Windows, Linux, MacOS). Limit je <strong>10
+            aktívnych zariadení na jednom účte</strong> a <strong>každé musí používať vlastnú jedinečnú verejnú IP adresu,</strong> ktorá <strong>nie je</strong>
+            označená ako <strong>VPN, Hosting a iné.</strong> Musí byť tzv. <strong>rezidentská,</strong> čo znamená od vášho poskytovateľa internetu
+            alebo mobilného operátora.
+        </p>
+
+        <div class="row">
+            <div class="col-md-6">
+                <h1 class="mt-5 mb-3">Graf mojich bodov:</h1>
+                <p class="text-muted">Začal som zarábať v polke dňa 9.4.2024 a používam jediné zariadenie (domáci linuxový
+                    server), ktoré beži <strong>24/7:</strong></p>
+                <img src="./assets/honeygain/graf.png" alt="Graf mojich bodov na HoneyGain" class="img-fluid mb-3">
+                <p class="text-muted text-center">Pre upresnenie <strong>100 bodov</strong> je <strong>10 centov,</strong> čo predstavuje prenesených <strong>500 MB.</strong></p>
+            </div>
+            <div class="col-md-6">
+                <div class="mt-5 card border-0 shadow-lg rounded" style="border-radius: 18px!important; background-color: #f7f7c1;">
+                    <div class="card-body">
+                        <h1 class="mb-3">Štartovací bonus</h1>
+                        <p class="text-muted d-inline" style="font-size: 1.25rem; color: black!important;">Ak chcete tiež začať zarábať s HoneyGain, tak môžete použiť dole nižšie <strong>štartovací kód:</strong></p>
+                        <a href="https://r.honeygain.me/SKEMG35220" target="_blank" class="d-inline ml-2">
+                            <button class="btn btn-warning text-dark" style="box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.15); border-radius: 18px!important;"><strong>Použiť Kód</strong></button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+<footer class="footer mt-auto py-3 bg-light">
+    <div class="container text-center">
+        <span class="text-muted">Táto stránka nie je sponzorovaná alebo reklamou na HoneyGain. Stránka slúži na poskytnutie informácií, či sa oplatí v dnešnej dobe HoneyGain na slovensku.</span>
+    </div>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+
+</html>
