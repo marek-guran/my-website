d3cfa3bb641cd08ab2efb7bab54c95d275998813
Marek Guráň <skemgi2@gmail.com>
2024-04-11 07:30:07 +0200

    update

--- honeygain.php
+++ honeygain.php
@@ -7,13 +7,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Honeygain na SK</title>
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="./assets/css/honeygain.css">
     <link href="fonts/fontawesome-free-6.5.1-web/css/all.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <meta name="description"
         content="Oplatí sa Honeygain na slovensku? Čo je to Honeygain? Ako zarobiť pasívne s Honeygain?">
-    <meta name="keywords" content="Honeygain, Slovensko, pasívny income, peniaze, zárobok">
+    <meta name="keywords" content="Honeygain, Slovensko, pasívny income, peniaze, zárobok, ByteLixir">
 </head>
 
 <body class="bg-light">
@@ -56,24 +55,65 @@
 
         <div class="row">
             <div class="col-md-6">
-                <h1 class="mt-5 mb-3">Graf mojich bodov:</h1>
+                <h1 class="mt-5 mb-3">Graf bodov:</h1>
                 <p class="text-muted">Začal som zarábať v polke dňa 9.4.2024 a používam jediné zariadenie (domáci linuxový
                     server), ktoré beží <strong>24/7:</strong></p>
                 <img src="./assets/honeygain/graf.png" alt="Graf mojich bodov na Honeygain" class="img-fluid mb-3">
-                <p class="text-muted text-center">Pre upresnenie <strong>100 bodov</strong> je <strong>10 centov,</strong> čo predstavuje prenesených <strong>500 MB.</strong></p>
+                <p class="text-muted text-center">Pre upresnenie <strong>100 bodov</strong> je <strong>10 centov,</strong> čo predstavuje prenesených <strong>500 MB.</strong>
+                <br>Vďaka <strong>JumpTask</strong> môžete získavať <strong>permanentný bonus 10%</strong> k vašim bodom (reprezentované žltou farbou na grafe).
+            </p>
             </div>
             <div class="col-md-6">
                 <div class="mt-5 card border-0 shadow-lg rounded" style="border-radius: 18px!important; background-color: #f7f7c1;">
                     <div class="card-body">
-                        <h1 class="mb-3">Štartovací bonus</h1>
+                        <h2 class="mb-1">Honeygain štartovací bonus 3$</h2>
                         <p class="text-muted d-inline" style="font-size: 1.25rem; color: black!important;">Ak chcete tiež začať zarábať s Honeygain, tak môžete použiť <strong>štartovací kód:</strong></p>
                         <a href="https://r.honeygain.me/SKEMG35220" target="_blank" class="d-inline ml-2">
                             <button class="btn btn-warning text-dark" style="box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.15); border-radius: 18px!important;"><strong>Použiť Kód</strong></button>
                         </a>
                     </div>
                 </div>
+                <div class="mt-3 card border-0 shadow-lg rounded" style="border-radius: 18px!important; background-color: #9ec3ff;">
+                    <div class="card-body">
+                        <h2 class="mb-1">JumpTask štartovací bonus</h2>
+                        <p class="text-muted d-inline" style="font-size: 1.25rem; color: black!important;">Bonusový kód vám dá <strong>10% bonus</strong> na splnenie prvej úlohy:</p>
+                        <a href="https://www.jumptask.io/r/gexukevobunu" target="_blank" class="d-inline ml-2">
+                            <button class="btn text-light" style="box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.15); border-radius: 18px!important; background-color: #394f73!important;"><strong>Použiť Kód</strong></button>
+                        </a>
+                    </div>
+                </div>
+                <div class="mt-3 card border-0 shadow-lg rounded" style="border-radius: 18px!important; background-color:#dec0fc;">
+                    <div class="card-body">
+                        <h2 class="mb-1">ByteLixir štartovací bonus</h2>
+                        <p class="text-muted d-inline" style="font-size: 1.25rem; color: black!important;">Momentálne sa dá získať <strong>bonus 1$:</strong></p>
+                        <a href="https://accounts.binance.com/register?ref=879453858" target="_blank" class="d-inline ml-2">
+                            <button class="btn text-light" style="box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.15); border-radius: 18px!important; background-color: #563973;"><strong>Použiť Kód</strong></button>
+                        </a>
+                    </div>
+                </div>
+                <div class="mt-3 card border-0 shadow-lg rounded" style="border-radius: 18px!important; background-color: #fce1c0;">
+                    <div class="card-body">
+                        <h2 class="mb-1">Binance registračný kód</h2>
+                        <p class="text-muted d-inline" style="font-size: 1.25rem; color: black!important;">Registračný kód:</p>
+                        <a href="https://accounts.binance.com/register?ref=879453858" target="_blank" class="d-inline ml-2">
+                            <button class="btn text-light" style="box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.15); border-radius: 18px!important; background-color: #b59c7d;"><strong>Použiť Kód</strong></button>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
+        <h1 class="mt-5 mb-3">Alternatíva k Honeygain</h1>
+        <p class="text-muted">Jednou z možných alternatív je služba <strong>ByteLixir,</strong> ktorá ponúka:<br>
+            <ul>
+                <li><strong>0.85$ / 1GB</strong> mobilné dáta</li>
+                <li><strong>0.68$ / 1GB</strong> domáca (rezidentská) IP adresa</li>
+                <li><strong>0.25$ / 1GB</strong> hostingové služby</li>
+            </ul>
+            <br>
+            Má odosť väčšie zisky ako Honeygain, ale je dostupná ibe pre Windows a Android zariadenia (prípadne aj MacOS a Linux po manuálnom nastavení proxy servera).<br>
+            <br>
+            Avšak, aby ste si mohli vybrať peniaze, musíte mať zarobených miniálne 5$ a môžete si to vybrať iba na krypto peňaženku (napr. Binance). Službu môžete používať zarovno s <strong>Honeygain</strong> a zväčšiť si tak svoje zárobky.
+        </p>
     </div>
 </body>
 <footer class="footer mt-auto py-3 bg-light">
